618962b4ea35628f2ca731affecd1b04e4d3b208
kiran-aut <kiran.sangha@outlook.com>
2023-01-04 14:20:50 +1300

    Checks for a password before copying

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+let passwordToCopy = "";
+
 //Generates a password given a length and whether or not you want uppercase, lowercase, digits, or symbols included
 function generatePassword(length, upper, lower, digits, symbols){
     
@@ -24,7 +26,7 @@ function generatePassword(length, upper, lower, digits, symbols){
         toJoin.push(symbolsAllowed);
     }
 
-    //If the length of the 
+    //If the length of the allowed string of characters is more than zero, generate a password of given length
     if(toJoin.length > 0){
         let charsAllowed = toJoin.join("");
         let password = [];
@@ -35,6 +37,7 @@ function generatePassword(length, upper, lower, digits, symbols){
         }
         
         document.getElementById("passwordAppearsHere").innerHTML = password.join("");
+        passwordToCopy = password.join("");
         return(password.join(""));
     }
     else{
@@ -43,7 +46,7 @@ function generatePassword(length, upper, lower, digits, symbols){
     }
 }
 
-
+//Listens for changes in checkbox state
 let uppercaseChars = true;
 let lowercaseChars = true;
 let digitChars = true;
@@ -65,6 +68,7 @@ document.getElementById("symbolChars").addEventListener("change", function(){
     symbolChars = !symbolChars;
 })
 
+//Listens for changes in the length slider and updates the length variable
 let sliderValue = 0;
 document.getElementById("slider").addEventListener("input", function(){
     let slider = document.getElementById("slider");
@@ -72,16 +76,19 @@ document.getElementById("slider").addEventListener("input", function(){
     sliderValue.innerHTML = slider.value;
 })
 
+//Listens for a click and calls the generatePassword function
 document.getElementById("generatePasswordButton").addEventListener("click", function(){
     document.getElementById("copyPassword").innerHTML = "Copy password to clipboard";
     generatePassword(slider.value,uppercaseChars,lowercaseChars,digitChars,symbolChars);
 });
 
+//If a password has been generated, listens for a click and copies the generated password to the clipboard
 document.getElementById("copyPassword").addEventListener("click", function(){
-    document.getElementById("copyPassword").innerHTML = "Copied password to clipboard";
-
-    let toCopy = document.getElementById("passwordAppearsHere");
-    navigator.clipboard.writeText(toCopy.textContent);
+    if(passwordToCopy.length > 0) {
+        navigator.clipboard.writeText(passwordToCopy);
+        document.getElementById("copyPassword").innerHTML = "Copied password to clipboard";
+    }
+    else{
+        document.getElementById("copyPassword").innerHTML = "No password generated";
+    }
 })
-
-
